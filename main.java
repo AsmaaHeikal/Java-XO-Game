25dc401514e56e03478c52ebbc6e9abfe2711e53
Sara_11 <92929722+sarawalid99@users.noreply.github.com>
2023-02-17 15:16:07 +0200

    Game Class "Commented until player class completed "

--- main.java
+++ main.java
@@ -1,5 +1,5 @@
-public class Main {
-    public static void main(String[] args) {
+public class main {
+    public static void Main(String[] args) {
         XO board = new XO();
         board.displayBoard(); 
 
